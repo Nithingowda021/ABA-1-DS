22ce91368c3defd16f0824c3aa258a837887841a
Nithin Kumar <52652008+Nithingowda021@users.noreply.github.com>
2021-11-30 10:45:27 +0530

    Add files via upload

---
+++ 4vv20cs075-ABA/prog2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+  int i,j,key,a[50],n;
+  printf("enter the no. of elements");
+{
+  scanf("%d",&n);
+}
+printf("enter the elements");
+{
+  for(i=0;i<n;i++)
+  {
+   scanf("%d",&a[i]);
+  }
+  for(i=1;i<n;i++)
+  {
+    key=a[i];
+    j=j-1;
+  }
+  a[j+1]==key;
+  printf("enter the array elements");
+  {
+    for(i=0;i<n;i++);
+    printf("%d",a[i]);
+   }
+  }
+}
+
+ 
