e47566144a3a8ad10d9b04cd49278b9d6f004aed
Nithin Kumar <52652008+Nithingowda021@users.noreply.github.com>
2021-11-30 10:08:42 +0530

    Add files via upload

---
+++ 4VV20Cs074-ABA/aba1.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+char str[100],pat[50],rep[50],ans[100];
+int i,j,k,m,c,flag=0;
+void stringmatch()
+{
+	i=j=k=m=c=0;
+	while(str[c]!='\0')
+	{
+		if(str[m]==pat[i])
+		{
+			i++;
+			m++;
+			if(pat[i]=='\0')
+			{
+				flag=1;
+				for(k=0;rep[k]!='\0';k++,j++)
+				{
+					ans[j]=rep[k];
+				}
+				i=0;
+				c=m;
+			}
+		}
+		else
+		{
+			ans[j]=str[c];
+			j++;
+			c++;
+			m=c;
+		}
+		ans[j]='\0';
+	}
+}
+	void main()
+	{
+		printf("enter a main string\n");
+		gets(str);
+		printf("enter a pattern string\n");
+		gets(pat);
+		printf("enter a replace string\n");
+		gets(rep);
+		stringmatch();
+		if(flag==1)
+		{
+			printf("the resultant string is %s",ans);
+		}
+		else
+		{
+			printf("pattern not found");
+		}
+	}	
