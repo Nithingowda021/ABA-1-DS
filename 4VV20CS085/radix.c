a07b3b455c0f7404c5a00e3525dffd0efc4fb77d
Nithin Kumar <52652008+Nithingowda021@users.noreply.github.com>
2021-12-01 14:02:03 +0530

    Add files via upload

---
+++ 4VV20CS085/radix.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int getmax(int arr[],int n)
+{
+int max=arr[0],i;
+for(i=1;i<n;i++)
+{
+if(arr[i]>max)
+{
+max=arr[i];
+}
+}
+return max;
+}
+int main()
+{
+int arr[10],i,n;
+printf("Enter the number of elements\n");
+scanf("%d",&n);
+printf("Enter the array elements\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+int m=getmax(arr,n);
+int exp;
+for(exp=1;m/exp>0;exp*=10)
+{
+int output[10];
+int count[10]={0};
+}
+for(i=1;i<n;i++)
+{
+count[arr[i]/exp%10]++;
+}
+for(i=1;i<10;i++)
+{
+count[i]=count[i]+count[i-1];
+}
+for(i=n-1;i>=0;i--)
+{
+output[count[arr[i]/exp%10]-1]=arr[i];
+count[arr[i]/exp%10]--;
+}
+for(i=0;i<n;i++)
+{
+printf("%d\t",output[i]);
+arr[i]=output[i];
+}
+printf("The sorted array is \n");
+for(i=0;i<n;i++)
+{
+printf("%d",arr[i]);
+}
+return 0;
+}
