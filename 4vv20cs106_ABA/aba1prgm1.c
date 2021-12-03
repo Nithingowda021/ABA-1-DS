22db32fe65636101715d5a702efd71792adb3400
Nithin Kumar <52652008+Nithingowda021@users.noreply.github.com>
2021-12-03 09:11:40 +0530

    Add files via upload

---
+++ 4vv20cs106_ABA/aba1prgm1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int i,j,r,c,count=0;
+printf("Enter the number of rows and columns ");
+scanf("%d%d",&r,&c);
+int *arr[r];
+for(i=0;i<r;i++)
+{
+arr[i]=(int*)malloc(c*sizeof(int));
+}
+printf("Enter the matrix elements");
+for(i=0;i<r;i++)
+{
+for(j=0;j<c;j++)
+{
+scanf("%d",&arr[i][j]);
+}
+}
+for(i=0;i<r;i++)
+{
+for(j=0;j<c;j++)
+{
+if(arr[i][j]==0)
+count++;
+}
+}
+if(count>((r*c)/2))
+printf("\nSparse matrix");
+else
+printf("\nNot a sparse matrix");
+}
