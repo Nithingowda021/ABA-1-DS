a7b13ff2d8b02da1623393c8d979377a68243304
Nithin Kumar <52652008+Nithingowda021@users.noreply.github.com>
2021-12-03 10:27:08 +0530

    Add files via upload

---
+++ 4VV20CS107_ABA/sparce.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int i,j,r,c,count=0;
+printf("enter the rows and columns:");
+scanf("%d%d",&r,&c);
+int *arr[r];
+for(i=0;i<r;i++)
+{
+arr[i]=(int*)malloc(c*sizeof(int));
+}
+for(i=0;i<r;i++)
+{
+for(j=0;j<c;j++)
+{
+scanf("%d",&arr[i][j]);
+}
+}
+for(i=0;i<r;i++)
+{
+for(j=0;j<c;j++)
+{
+if(arr[i][j]==0)
+count++;
+}
+}
+if(count>((r*c)/2))
+{
+printf("sparce matrix\n");
+}
+else
+{
+printf("not a sparce matrix\n");
+}
+free(arr);
+}
